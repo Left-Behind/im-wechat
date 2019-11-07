3bfb3701233c8f290c414508113f8babf04238e0
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-11-07 17:14:43 +0800

    :wrench:添加html页面，IndexController控制器，补充配置文件

---
+++ im-netty/src/main/java/work/azhu/imnetty/controller/IndexController.java
@@ -0,0 +1,36 @@
+package work.azhu.imnetty.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author Azhu
+ * @Date 2019/11/7 16:45
+ * @Description
+ */
+
+@Controller
+public class IndexController {
+
+
+
+    @Value("${netty.port}")
+    Long nettyPort;
+
+    @GetMapping("/index")
+    public String index(){
+        System.out.println("访问 ---> ws://www.azhu.work:8070/ws");
+        return "chat70";
+    }
+
+    @RequestMapping("/getNettyPort")
+    @ResponseBody
+    public String getSession() {
+        return "当前Netty服务器监听端口：" + nettyPort;
+    }
+}
