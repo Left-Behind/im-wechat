84d50996bef9780a8f378927661020e589617a98
Left-Behind <zhuwengjun19961002@foxmail.com>
2019-11-18 20:06:30 +0800

    :tada:添加数据库database模块,并实现服务注册，为其他模块提供操作数据库的接口

---
+++ im-database/src/main/java/work/azhu/imdatabase/DubboSeriveceImpl.java
@@ -0,0 +1,31 @@
+package work.azhu.imdatabase;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Value;
+import work.azhu.imcommon.DubboService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author Azhu
+ * @Date 2019/11/1 16:57
+ * @Description dubbo和springcloud中的feign不同，dubbo暴露的是service层接口
+ */
+@Service(version = "${demo.service.version}")
+public class DubboSeriveceImpl implements DubboService {
+
+    @Value("${server.port}")
+    Integer port;
+    public String sayHello(String name) {
+        return "微服务端口号："+port+"Hello, " + name + " (from Spring Boot)";
+    }
+
+    @HystrixCommand
+    public String welcome() {
+        return "这是一个通过zookeeper注册的服务";
+    }
+
+}
