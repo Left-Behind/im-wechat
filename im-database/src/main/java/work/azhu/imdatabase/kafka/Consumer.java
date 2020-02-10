426610d93e13bd9540190513b7bbca66e30b4b9b
Left-Behind <zhuge123Azhu>
2020-02-10 20:24:54 +0800

    :wine:消息队列调通

--- im-database/src/main/java/work/azhu/imdatabase/kafka/Consumer.java
+++ im-database/src/main/java/work/azhu/imdatabase/kafka/Consumer.java
@@ -1,9 +1,15 @@
 package work.azhu.imdatabase.kafka;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import work.azhu.imcommon.model.bean.common.Message;
+import work.azhu.imcommon.util.SnowflakeIdWorker;
+import work.azhu.imdatabase.mapper.MessageMapper;
 
 import java.util.Optional;
 
@@ -15,6 +21,10 @@ import java.util.Optional;
 @Component
 @Slf4j
 public class Consumer {
+
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @KafkaListener(topics = {"web"})
     public void listenWeb(ConsumerRecord<?, ?> record){
@@ -36,14 +46,17 @@ public class Consumer {
     public void listenDatabase(ConsumerRecord<?, ?> record){
 
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
+        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
 
         if (kafkaMessage.isPresent()) {
+            Object object = kafkaMessage.get();
+            JSONObject userJson = JSONObject.parseObject(object.toString());
+            Message message = JSON.toJavaObject(userJson,Message.class);
+            message.setId(idWorker.nextId());
 
-            Object message = kafkaMessage.get();
-            log.info("消费到了database模块的消息");
-            System.out.println("---->"+record);
-            System.out.println("---->"+message);
-
+            log.info("消费到了netty服务的消息");
+            log.info("插入数据库信息: "+object.toString());
+            messageMapper.insertMessage(message);
         }
 
     }
